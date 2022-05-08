b5e2ff6b4423ff0360dd7f373b86a23a6d0ccef2
Kalleo Leandro dos Santos Leal <kalleo.leal@gmail.com>
2022-05-08 11:44:18 -0300

    Tratamento das exceções de validação dos campos em post e update

---
+++ src/main/java/com/teste/api/resources/exception/ValidationError.java
@@ -0,0 +1,35 @@
+package com.teste.api.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kalleo
+ *
+ */
+public class ValidationError extends StandardError
+{
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError() 
+	{
+		super();
+	
+	}
+
+	public ValidationError(Long timestamp, Integer status, String error)
+	{
+		super(timestamp, status, error);
+	}
+
+	public List<FieldMessage> getErrors() 
+	{
+		return errors;
+	}
+
+	public void addErrors(String fieldName, String message) 
+	{
+		this.errors.add(new FieldMessage(fieldName,message));
+	}	
+}
